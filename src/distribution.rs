1f17ccfbb8676d7ef8ecad46d90cd3e9e9e07bb6
Ethan Jones <etn.jones@gmail.com>
2023-06-30 18:21:21 -0500

    RandomUnitVector.
    Sample each dimension from a StdNormal, compute magnitude, normalize.

--- src/distribution.rs
+++ src/distribution.rs
@@ -1,14 +1,42 @@
 //! Generate random unit vectors to serve as hyperplane normals.
-use rand::Rng;
+use rand::{Fill, Rng};
+use rand_distr::{Distribution, StandardNormal};
 
 pub trait RandomUnitVector<const D: usize> {
     type Output;
     fn sample<R: Rng>(rng: &mut R) -> Self::Output;
 }
 
+// TODO: This would capture [f32, f64], but requires some unsafe array-initialization due to the
+// const-generic.
+//impl<const D: usize, T> RandomUnitVector<D> for T
+//where
+//    StandardNormal: Distribution<T>,
+//{
+//    type Output = [Self; D];
+//    fn sample<R: Rng>(rng: &mut R) -> Self::Output {
+//        todo!();
+//    }
+//}
+
 impl<const D: usize> RandomUnitVector<D> for f32 {
-    type Output = [f32; D];
+    type Output = [Self; D];
     fn sample<R: Rng>(rng: &mut R) -> Self::Output {
-        todo!();
+        // ∑(xi^2) |> sqrt
+        let mag = |x: &[Self]| -> Self { x.iter().map(|&xi| xi * xi).sum::<Self>().sqrt() };
+
+        let mut out = [0.0_f32; D];
+        for mem in out.iter_mut() {
+            *mem = rng.sample(StandardNormal);
+        }
+
+        let out_mag = mag(&out);
+        assert!(out_mag > 0.0_f32);
+
+        for mem in out.iter_mut() {
+            *mem = *mem / out_mag;
+        }
+
+        out
     }
 }
--- src/ident.rs
+++ src/ident.rs
@@ -1,3 +1,5 @@
+use super::lsh::IdentifierMethod;
+
 /// Given a usize bin, return another bin that is 1-hamming distance away.
 pub(super) fn similar_bin<const N: usize>(bin: usize, ident: &IdentifierMethod) -> usize {
     todo!();
