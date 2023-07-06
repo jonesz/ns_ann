0d9a4f87c97e504ea21f3a7b88f79475d28786c7
Ethan Jones <etn.jones@gmail.com>
2023-07-06 11:17:11 -0500

    Utilize num_traits to provide a generic impl for RandomUnitVector.

--- src/distribution.rs
+++ src/distribution.rs
@@ -1,6 +1,7 @@
 //! Generate random unit vectors to serve as hyperplane normals.
+use num_traits::real::Real;
 use rand::Rng;
-use rand_distr::StandardNormal;
+use rand_distr::{Distribution, StandardNormal};
 
 pub fn build_random_unit_hyperplanes<const N: usize, T, const D: usize, R>(
     rng: &mut R,
@@ -22,31 +23,23 @@ pub trait RandomUnitVector<const D: usize> {
     fn sample<R: Rng>(rng: &mut R) -> Self::Output;
 }
 
-// TODO: This would capture [f32, f64], but requires some unsafe array-initialization due to the
-// const-generic.
-//impl<const D: usize, T> RandomUnitVector<D> for T
-//where
-//    StandardNormal: Distribution<T>,
-//{
-//    type Output = [Self; D];
-//    fn sample<R: Rng>(rng: &mut R) -> Self::Output {
-//        todo!();
-//    }
-//}
-
-impl<const D: usize> RandomUnitVector<D> for f32 {
+impl<const D: usize, T> RandomUnitVector<D> for T
+where
+    StandardNormal: Distribution<T>,
+    T: Real + Default + core::ops::DivAssign + core::iter::Sum,
+{
     type Output = [Self; D];
     fn sample<R: Rng>(rng: &mut R) -> Self::Output {
         // ∑(xi^2) |> sqrt
         let mag = |x: &[Self]| -> Self { x.iter().map(|&xi| xi * xi).sum::<Self>().sqrt() };
 
-        let mut out = [0.0_f32; D];
+        let mut out = [T::default(); D];
         for mem in out.iter_mut() {
             *mem = rng.sample(StandardNormal);
         }
 
         let out_mag = mag(&out);
-        assert!(out_mag > 0.0_f32);
+        assert!(out_mag > T::default());
 
         for mem in out.iter_mut() {
             *mem /= out_mag;
